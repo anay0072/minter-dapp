734a2bee4116f22ffabb762fb4a9bf97f412f00b
anay <anay200412sagar@gmail.com>
2022-03-29 15:12:03 +0530

    commit

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,22 +7,21 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
+const namePrefix = "Doodle Panther";
+const description = "Doodle Panther is a collection of 1000 randomly generated and stylistically curated NFTs that exist on the Ethereum Blockchain. The Doodle Panther NFT is a key to the next big project by the panther community. Doodle Panther holders can participate in exclusive events such as NFT claims, raffles, community giveaways, and more.";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 1000,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Clothes" },
+      { name: "Eyes" },
+      { name: "Hat" },
+      { name: "Head" },
+      { name: "Mouth" },
     ],
   },
 ];
@@ -32,13 +31,13 @@ const shuffleLayerConfigurations = true;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 2000,
+  height: 2000,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://DoodlePanther.com", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
